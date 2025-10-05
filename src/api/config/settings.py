0312c6577bcfec01eff7fb01f09e5c87332cc457
Tim <lambchop4prez@users.noreply.github.com>
2025-10-05 15:25:15 -0400

    fix(deps): Fixing pinned python version

--- src/api/config/settings.py
+++ src/api/config/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    # model_config = SettingsConfigDict(env_file=".env", env_prefix="BACKEND_")
+    model_config = SettingsConfigDict(env_file=".env", env_prefix="BACKEND_")
     API_VERSION: Literal["v1"] = "v1"
     REPOSITORY: Literal["inmemory", "mongodb"] = "inmemory"
     PROJECT_NAME: str = "FMV Demo"
