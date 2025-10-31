39f7ccc4fe55aab715505a6876f5cd5a6082f549
Tim <lambchop4prez@users.noreply.github.com>
2025-10-31 17:31:16 -0400

    fix: Add default value, remove unused value

--- src/pkg/config/src/config/mongo.py
+++ src/pkg/config/src/config/mongo.py
@@ -5,8 +5,7 @@ class MongoSettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_prefix="BACKEND_MONGO_", extra="allow"
     )
-    HOST: str
-    DATABASE: str
+    HOST: str = "localhost"
 
 
 settings = MongoSettings()
