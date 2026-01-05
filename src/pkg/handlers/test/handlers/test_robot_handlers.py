4fde53bfc560984e7be3a98dcf5c4b3ba4a901e4
Tim <lambchop4prez@users.noreply.github.com>
2026-01-05 16:09:03 -0500

    ci: Fix test, try to remove manual python install

--- src/pkg/handlers/test/handlers/test_robot_handlers.py
+++ src/pkg/handlers/test/handlers/test_robot_handlers.py
@@ -3,5 +3,5 @@ from handlers import RobotHandlers
 
 def test_primes() -> None:
     subject = RobotHandlers("test")
-    result = subject.primes(3)
+    result = subject.primes(5)
     assert result == [1, 3, 5]
