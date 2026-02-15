f5285c37bb4eb0f1a0204d1c3ef1f396c2886cb8
Tim <lambchop4prez@users.noreply.github.com>
2026-02-14 22:08:28 -0500

    test: Move test setup

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "cypress";
 import vitePreprocessor from 'cypress-vite';
 
-interface RobotProfile {
-  name: string;
-  is_great: boolean;
-  description: string;
-  location: string;
-}
 export default defineConfig({
   downloadsFolder: 'test/downloads',
   fixturesFolder: 'test/fixtures',
@@ -19,16 +13,7 @@ export default defineConfig({
     setupNodeEvents(on/*, config*/) {
       // implement node event listeners here
       on('file:preprocessor', vitePreprocessor());
-      on('before:run', (/*details*/) => {
-        cy.fixture('seed').then((data: Array<RobotProfile>) => {
-          data.forEach((element: RobotProfile) => {
-            cy.task('httpRequest', {
-              url: 'http://localhost:8000/v1/robot/',
-              body: element
-            });
-          });
-        });
-      });
+
     },
   },
 });
--- frontend/test/e2e/pages/robot/index.spec.ts
+++ frontend/test/e2e/pages/robot/index.spec.ts
@@ -1,4 +1,16 @@
+import type { components } from '~/client/api';
+type RobotProfile = components['schemas']['RobotProfile'];
 describe('Robot List', () => {
+  before(() => {
+    cy.fixture('seed').then((data: Array<RobotProfile>) => {
+      data.forEach((element: RobotProfile) => {
+        cy.task('httpRequest', {
+          url: 'http://localhost:8000/v1/robot/',
+          body: element
+        });
+      });
+    });
+  })
   it('should display a collection of robots', () => {
     cy.visit('/robot');
     cy.get('.robot-item').should('be.visible');
