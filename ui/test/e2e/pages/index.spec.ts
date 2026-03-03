e7e7759f1625f3ec2591a7134eac92ac72d49050
Tim <lambchop4prez@users.noreply.github.com>
2026-03-03 10:14:42 -0500

    test: Updates for e2e testing

--- ui/test/e2e/pages/index.spec.ts
+++ ui/test/e2e/pages/index.spec.ts
@@ -1,8 +1,8 @@
 describe('The main page', () => {
   it('displays the Go button.', function () {
     cy.visit("/");
-    cy.get('button#go').should('be.visible');
-    cy.get('button#go').click();
+    cy.get('#go').should('be.visible');
+    cy.get('#go').click();
     cy.url().should('include', '/robot');
   });
 });
