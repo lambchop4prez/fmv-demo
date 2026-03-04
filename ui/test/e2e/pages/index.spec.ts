57cdea3aa1d5c5c2441574f47d64020a9454c127
Tim <lambchop4prez@users.noreply.github.com>
2026-03-04 13:45:27 -0500

    test: Remove more cypress

--- ui/test/e2e/pages/index.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('The main page', () => {
-  it('displays the Go button.', function () {
-    cy.visit("/");
-    cy.get('#go').should('be.visible');
-    cy.get('#go').click();
-    cy.url().should('include', '/robot');
-  });
-});
