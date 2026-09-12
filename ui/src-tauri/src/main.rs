bcfeb0f11ca3e816d2c82aa994a69b1a1644584c
Tim <lambchop4prez@users.noreply.github.com>
2026-09-12 10:48:01 -0400

    style: Run cargo fmt

--- ui/src-tauri/src/main.rs
+++ ui/src-tauri/src/main.rs
@@ -2,8 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  if let Err(e) = app_lib::run() {
-    eprintln!("Application error {e}");
-    std::process::exit(1);
-  }
+    if let Err(e) = app_lib::run() {
+        eprintln!("Application error {e}");
+        std::process::exit(1);
+    }
 }
